da462a142dc0567a91a7c5b636c8a000b33092dd
Tom Hunze <github@thunze.de>
2023-04-03 21:04:03 +0200

    Add tests for the module `fat.base`

---
+++ tests/fat/test_base.py
@@ -0,0 +1,20 @@
+"""Tests for the ``base`` module of the ``fat`` package."""
+
+import pytest
+
+from diskfs.fat.base import FatType
+from diskfs.filesystem import FsType
+
+
+@pytest.mark.parametrize(
+    ['fs_type', 'fat_type'],
+    [
+        (FsType.FAT_12, FatType.FAT_12),
+        (FsType.FAT_16, FatType.FAT_16),
+        (FsType.FAT_32, FatType.FAT_32),
+    ],
+)
+def test_fat_type_mapping(fs_type, fat_type):
+    """Test mapping between ``FatType`` and ``FsType``."""
+    assert FatType.from_fs_type(fs_type) is fat_type
+    assert fat_type.fs_type is fs_type
