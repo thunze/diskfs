2c7dce197f8a38662c22b09ad52a9227f8efa73b
Tom Hunze <github@thunze.de>
2023-04-16 16:40:59 +0200

    Add tests for `BpbDos331`

--- tests/fat/test_reserved.py
+++ tests/fat/test_reserved.py
@@ -7,9 +7,12 @@ import pytest
 from diskfs.base import SectorSize, ValidationError
 from diskfs.fat.reserved import (
     CLUSTER_SIZE_DEFAULT,
+    HEADS_DEFAULT,
     MEDIA_TYPE_DEFAULT,
     ROOTDIR_ENTRIES_DEFAULT,
+    SECTORS_PER_TRACK_DEFAULT,
     BpbDos200,
+    BpbDos331,
 )
 from diskfs.volume import Volume
 
@@ -23,7 +26,7 @@ def volume_basic(request, monkeypatch):
     ``end_lba`` and ``sector_size``.
     """
     start, end, lss, pss = request.param
-    size = end - start
+    size = end - start + 1
     monkeypatch.setattr('diskfs.volume.Volume.__init__', lambda self: None)
     monkeypatch.setattr('diskfs.volume.Volume.start_lba', property(lambda self: start))
     monkeypatch.setattr('diskfs.volume.Volume.end_lba', property(lambda self: end))
@@ -37,15 +40,16 @@ def volume_basic(request, monkeypatch):
 # TODO: Test BPBs used for formatting
 
 # Example BPBs with sane default values
+
 BPB_DOS_200_FAT12_EXAMPLE = BpbDos200(
     lss=512,
     cluster_size=CLUSTER_SIZE_DEFAULT,
     reserved_size=1,
     fat_count=1,
     rootdir_entries=ROOTDIR_ENTRIES_DEFAULT,  # 15 sectors
-    total_size_200=16384,  # 1 + 3 + 15 + 16365 sectors
+    total_size_200=40960,  # 1 + 8 + 15 + 40936 sectors
     media_type=MEDIA_TYPE_DEFAULT,
-    fat_size_200=3,
+    fat_size_200=8,
 )
 BPB_DOS_200_FAT16_EXAMPLE = BpbDos200(
     lss=512,
@@ -62,10 +66,32 @@ BPB_DOS_200_FAT32_EXAMPLE = BpbDos200(
     cluster_size=CLUSTER_SIZE_DEFAULT,
     reserved_size=6,
     fat_count=2,
-    rootdir_entries=0,
+    rootdir_entries=0,  # 0 sectors
     total_size_200=0,
     media_type=MEDIA_TYPE_DEFAULT,
     fat_size_200=0,
+)
+
+BPB_DOS_331_FAT12_EXAMPLE = BpbDos331(
+    bpb_dos_200_=BPB_DOS_200_FAT12_EXAMPLE,
+    sectors_per_track=SECTORS_PER_TRACK_DEFAULT,
+    heads=HEADS_DEFAULT,
+    hidden_before_partition=0,
+    total_size_331=0,
+)
+BPB_DOS_331_FAT16_EXAMPLE = BpbDos331(
+    bpb_dos_200_=BPB_DOS_200_FAT16_EXAMPLE,
+    sectors_per_track=SECTORS_PER_TRACK_DEFAULT,
+    heads=HEADS_DEFAULT,
+    hidden_before_partition=0,
+    total_size_331=131072,  # 2 + 2 * 32 + 15 + 130991 sectors
+)
+BPB_DOS_331_FAT32_EXAMPLE = BpbDos331(
+    bpb_dos_200_=BPB_DOS_200_FAT32_EXAMPLE,
+    sectors_per_track=SECTORS_PER_TRACK_DEFAULT,
+    heads=HEADS_DEFAULT,
+    hidden_before_partition=0,
+    total_size_331=2097152,  # 6 + 2 * 1024 + 0 + 2095098 sectors
 )
 
 
@@ -165,12 +191,12 @@ class TestBpbDos200:
     @pytest.mark.parametrize(
         ['volume_basic', 'replace_kwargs'],
         [
-            ((0, 2048, 512, 512), {'lss': 512, 'rootdir_entries': 16}),
-            ((0, 2048, 512, 4096), {'lss': 512, 'rootdir_entries': 16}),
-            ((0, 2048, 4096, 4096), {'lss': 4096, 'rootdir_entries': 128}),
-            ((0, 2048, 512, 512), {'total_size_200': 1024}),
-            ((0, 2048, 512, 512), {'total_size_200': 2048}),
-            ((0, 4096, 512, 512), {'total_size_200': 2048}),
+            ((0, 2047, 512, 512), {'lss': 512, 'rootdir_entries': 16}),
+            ((0, 2047, 512, 4096), {'lss': 512, 'rootdir_entries': 16}),
+            ((0, 2047, 4096, 4096), {'lss': 4096, 'rootdir_entries': 128}),
+            ((0, 2047, 512, 512), {'total_size_200': 1024}),
+            ((0, 2047, 512, 512), {'total_size_200': 2048}),
+            ((0, 4095, 512, 512), {'total_size_200': 1001}),
         ],
         indirect=['volume_basic'],
     )
@@ -192,8 +218,8 @@ class TestBpbDos200:
                 {'lss': 4096, 'rootdir_entries': 128},
                 'Logical sector size.*disk',
             ),
-            ((0, 2048, 512, 512), {'total_size_200': 4096}, 'Total size'),
-            ((0, 4096, 512, 512), {'total_size_200': 4097}, 'Total size'),
+            ((0, 2047, 512, 512), {'total_size_200': 4096}, 'Total size.*volume'),
+            ((0, 4095, 512, 512), {'total_size_200': 4097}, 'Total size.*volume'),
         ],
         indirect=['volume_basic'],
     )
@@ -204,15 +230,149 @@ class TestBpbDos200:
             bpb.validate_for_volume(volume_basic)
 
     @pytest.mark.parametrize(
-        ['bpb', 'expected_total_size'],
+        ['bpb', 'total_size'],
         [
-            (BPB_DOS_200_FAT12_EXAMPLE, 16384),
+            (BPB_DOS_200_FAT12_EXAMPLE, 40960),
             (BPB_DOS_200_FAT16_EXAMPLE, None),
             (BPB_DOS_200_FAT32_EXAMPLE, None),
         ],
     )
-    def test_properties(self, bpb, expected_total_size):
+    def test_properties(self, bpb, total_size):
         """Test that property values defined on BPBs match the expected values."""
         assert bpb.bpb_dos_200 is bpb
         assert bpb.fat_size == bpb.fat_size_200
-        assert bpb.total_size == expected_total_size
+        assert bpb.total_size == total_size
+
+
+class TestBpbDos331:
+    """Tests for ``BpbDos331``."""
+
+    @pytest.mark.parametrize(
+        'replace_kwargs',
+        [
+            {'sectors_per_track': 63},
+            {'sectors_per_track': 26},
+            {'heads': 255},
+            {'heads': 2},
+            {
+                'bpb_dos_200_': replace(BPB_DOS_200_FAT12_EXAMPLE, total_size_200=8192),
+                'total_size_331': 8192,
+            },
+            {
+                'bpb_dos_200_': replace(BPB_DOS_200_FAT12_EXAMPLE, total_size_200=0),
+                'total_size_331': 8192,
+            },
+            {
+                'bpb_dos_200_': replace(BPB_DOS_200_FAT12_EXAMPLE, total_size_200=8192),
+                'total_size_331': 0,
+            },
+            {
+                'bpb_dos_200_': replace(BPB_DOS_200_FAT32_EXAMPLE, total_size_200=0),
+                'total_size_331': 0,
+            },
+        ],
+    )
+    def test_validate_success(self, replace_kwargs):
+        """Test custom validation logic for succeeding cases."""
+        replace(BPB_DOS_331_FAT16_EXAMPLE, **replace_kwargs)
+
+    @pytest.mark.parametrize(
+        ['replace_kwargs', 'msg_contains'],
+        [
+            ({'sectors_per_track': 64}, 'Sector count per track'),
+            ({'sectors_per_track': 65}, 'Sector count per track'),
+            ({'heads': 256}, 'Head count'),
+            ({'heads': 257}, 'Head count'),
+            (
+                {
+                    'bpb_dos_200_': replace(
+                        BPB_DOS_200_FAT12_EXAMPLE, total_size_200=4096
+                    ),
+                    'total_size_331': 8192,
+                },
+                r'Total size.*2\.0',
+            ),
+            (
+                {
+                    'bpb_dos_200_': replace(
+                        BPB_DOS_200_FAT12_EXAMPLE, total_size_200=8192
+                    ),
+                    'total_size_331': 4096,
+                },
+                r'Total size.*2\.0',
+            ),
+        ],
+    )
+    def test_validate_fail(self, replace_kwargs, msg_contains):
+        """Test custom validation logic for failing cases."""
+        with pytest.raises(ValidationError, match=f'.*{msg_contains}.*'):
+            replace(BPB_DOS_331_FAT16_EXAMPLE, **replace_kwargs)
+
+    @pytest.mark.parametrize(
+        ['volume_basic', 'replace_kwargs'],
+        [
+            (
+                (0, BPB_DOS_331_FAT16_EXAMPLE.total_size_331 - 1, 512, 512),
+                {'hidden_before_partition': 0},
+            ),
+            (
+                (2048, BPB_DOS_331_FAT16_EXAMPLE.total_size_331 + 2047, 512, 512),
+                {'hidden_before_partition': 2048},
+            ),
+            (
+                (7, BPB_DOS_331_FAT16_EXAMPLE.total_size_331 + 6, 512, 512),
+                {'hidden_before_partition': 7},
+            ),
+            ((0, 2047, 512, 512), {'total_size_331': 1024}),
+            ((0, 2047, 512, 512), {'total_size_331': 2048}),
+            ((0, 4095, 512, 512), {'total_size_331': 1001}),
+            ((0, 99999, 512, 512), {'total_size_331': 100000}),
+            ((0, 100999, 512, 512), {'total_size_331': 100000}),
+        ],
+        indirect=['volume_basic'],
+    )
+    def test_validate_for_volume_success(self, volume_basic, replace_kwargs):
+        """Test validation against a specific volume for succeeding cases."""
+        bpb = replace(BPB_DOS_331_FAT16_EXAMPLE, **replace_kwargs)
+        bpb.validate_for_volume(volume_basic)
+
+    @pytest.mark.parametrize(
+        ['volume_basic', 'replace_kwargs', 'msg_contains'],
+        [
+            (
+                (0, 2048, 4096, 512),
+                {
+                    'bpb_dos_200_': replace(
+                        BPB_DOS_200_FAT16_EXAMPLE, lss=512, rootdir_entries=16
+                    )
+                },
+                'Logical sector size.*disk',
+            ),
+            ((0, 3, 512, 512), {'hidden_before_partition': 8}, 'Hidden sector'),
+            ((0, 2047, 512, 512), {'hidden_before_partition': 2048}, 'Hidden sector'),
+            ((0, 2047, 512, 512), {'total_size_331': 4096}, 'Total size.*volume'),
+            ((0, 4095, 512, 512), {'total_size_331': 4097}, 'Total size.*volume'),
+            ((0, 99999, 512, 512), {'total_size_331': 101000}, 'Total size.*volume'),
+        ],
+        indirect=['volume_basic'],
+    )
+    def test_validate_for_volume_fail(self, volume_basic, replace_kwargs, msg_contains):
+        """Test validation against a specific volume for failing cases."""
+        bpb = replace(BPB_DOS_331_FAT16_EXAMPLE, **replace_kwargs)
+        with pytest.raises(ValidationError, match=f'.*{msg_contains}.*'):
+            bpb.validate_for_volume(volume_basic)
+
+    @pytest.mark.parametrize(
+        ['bpb', 'total_size'],
+        [
+            (BPB_DOS_331_FAT12_EXAMPLE, 40960),
+            (BPB_DOS_331_FAT16_EXAMPLE, 131072),
+            (BPB_DOS_331_FAT32_EXAMPLE, 2097152),
+            (replace(BPB_DOS_331_FAT32_EXAMPLE, total_size_331=0), None),
+        ],
+    )
+    def test_properties(self, bpb, total_size):
+        """Test that property values defined on BPBs match the expected values."""
+        assert bpb.bpb_dos_200 is bpb.bpb_dos_200_
+        assert bpb.fat_size == bpb.bpb_dos_200_.fat_size_200
+        assert bpb.total_size == total_size
